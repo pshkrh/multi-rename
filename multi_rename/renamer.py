28c4f0d654a21dab58feed36b835ee15e8afefec
Pushkar Kurhekar <pshkrh@gmail.com>
2020-06-22 00:48:09 +0530

    Add suffix function

--- multi_rename/renamer.py
+++ multi_rename/renamer.py
@@ -17,6 +17,22 @@ def add_prefix(dir_path=None, prefix=None, sep=''):
                 repl_path = os.path.join(dir_path, repl_name)
                 os.replace(file_path, repl_path)
 
+def add_suffix(dir_path=None, suffix=None, sep=''):
+    if dir_path is not None and suffix is not None:
+        if not dir_path.endswith(os.path.sep):
+            dir_path += os.path.sep
+
+        file_list = os.listdir(dir_path)
+
+        for file_str in file_list:
+            file_path = os.path.join(dir_path, file_str)
+            file_name, extension = os.path.splitext(file_str)
+
+            if not os.path.isdir(file_path):
+                repl_name = file_name + sep + suffix + extension
+                repl_path = os.path.join(dir_path, repl_name)
+                os.replace(file_path, repl_path)
+
 def full_rename(dir_path=None, new_name=None, idx=1, increment=1, sep=''):
 
     if None not in (dir_path, new_name):
