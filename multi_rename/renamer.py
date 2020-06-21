37e476156b0f3620712623e2923c0a8d2eb91fd6
Pushkar Kurhekar <pshkrh@gmail.com>
2020-06-21 23:48:08 +0530

    Handle paths without trailing slash

--- multi_rename/renamer.py
+++ multi_rename/renamer.py
@@ -4,7 +4,11 @@ import logging
 def full_rename(dir_path=None, new_name=None, idx=1, increment=1):
 
     if None not in (dir_path,new_name):
+        if not dir_path.endswith(os.path.sep):
+            dir_path += os.path.sep
+
         file_list = os.listdir(dir_path)
+
         for file in file_list:
             file_name = dir_path + os.path.splitext(file)[0] + os.path.splitext(file)[1]
             extension = os.path.splitext(file)[1]
@@ -13,6 +17,7 @@ def full_rename(dir_path=None, new_name=None, idx=1, increment=1):
                 repl_name = dir_path + new_name + str(idx) + extension
                 try:
                     os.rename(file_name,repl_name)
+                    logging.info('File name changed from {} to {}'.format(file_name, repl_name))
                 except:
                     logging.basicConfig(filename='batcher.log', filemode='w', format='%(name)s - %(levelname)s - %(message)s')
                     logging.warning('The file {} already exists.'.format(file_name))
