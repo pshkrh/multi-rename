d13f790ad1fff9c893ec0a91180aea5adb12e84c
Pushkar Kurhekar <pshkrh@gmail.com>
2020-06-22 22:09:42 +0530

    Add docstrings

--- multi_rename/renamer.py
+++ multi_rename/renamer.py
@@ -2,6 +2,25 @@ import os
 import logging
 
 def add_affix(dir_path=None, affix=None, affix_type=None, sep='', filter_ext=[]):
+    """Adds an affix to the existing file names in a directory
+
+    Parameters
+    ----------
+    dir_path : str
+        The directory which contains the files that need to be renamed
+    affix: str
+        The affix string which will be added to the existing
+        file name
+    affix_type: str
+        The type of affix to be used. Possible values: prefix, suffix
+    sep: str, optional
+        Separator string to be used between the file name and affix
+        (default is empty string)
+    filter_ext : list of str, optional
+        List of file extensions that should be ignored
+        (default is an empty list)
+    """
+
     if dir_path is not None and affix is not None:
         if not dir_path.endswith(os.path.sep):
             dir_path += os.path.sep
@@ -25,6 +44,29 @@ def add_affix(dir_path=None, affix=None, affix_type=None, sep='', filter_ext=[])
                 os.replace(file_path, repl_path)
 
 def full_rename(dir_path=None, new_name=None, idx=1, increment=1, sep='', filter_ext=[]):
+    """Renames all the files in a directory with a new name
+    with incrementing numbers
+
+    Parameters
+    ----------
+    dir_path : str
+        The directory which contains the files that need to be renamed
+    new_name: str
+        The new name for all the files in the directory
+    idx: int
+        Start value of the trailing number for the file name
+        (default is 1)
+    increment: int
+        The amount by which to increment the trailing number
+        (default is 1)
+    sep: str, optional
+        Separator string to be used between the file name and affix
+        (default is an empty string)
+    filter_ext : list of str, optional
+        List of file extensions that should be ignored
+        (default is an empty list)
+    """
+
     if dir_path is not None and new_name is not None:
         if not dir_path.endswith(os.path.sep):
             dir_path += os.path.sep
