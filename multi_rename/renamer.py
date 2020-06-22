67dbc27a658410313e25dda5bf66694f702d9187
Pushkar Kurhekar <pshkrh@gmail.com>
2020-06-22 21:40:28 +0530

    Add extension filter

--- multi_rename/renamer.py
+++ multi_rename/renamer.py
@@ -33,8 +33,7 @@ def add_suffix(dir_path=None, suffix=None, sep=''):
                 repl_path = os.path.join(dir_path, repl_name)
                 os.replace(file_path, repl_path)
 
-def full_rename(dir_path=None, new_name=None, idx=1, increment=1, sep=''):
-
+def full_rename(dir_path=None, new_name=None, idx=1, increment=1, sep='', filter_ext=[]):
     if dir_path is not None and new_name is not None:
         if not dir_path.endswith(os.path.sep):
             dir_path += os.path.sep
@@ -44,6 +43,9 @@ def full_rename(dir_path=None, new_name=None, idx=1, increment=1, sep=''):
         for file_str in file_list:
             file_path = os.path.join(dir_path, file_str)
             extension = os.path.splitext(file_str)[1]
+
+            if filter_ext and extension[1:] in filter_ext:
+                continue
 
             if not os.path.isdir(file_path):
                 repl_name = new_name + sep + str(idx) + extension
