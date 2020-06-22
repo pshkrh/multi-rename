637334336cb31a2e685b1dee38070fa8ea135b5d
Pushkar Kurhekar <pshkrh@gmail.com>
2020-06-22 22:32:01 +0530

    Bump version number to 0.0.2

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="multi-rename",
-    version="0.0.1",
+    version="0.0.2",
     author="Pushkar Kurhekar",
     author_email="dev@pshkrh.com",
     description="Easily rename multiple files ending with incrementing numbers.",
